636714f1d512231e665ad352115706cf250e8879
tendeanclaudior <tendeanclaudio1234@gmail.com>
2023-12-09 18:39:14 +0700

    IntegrationAPIHistory

--- src/Pages/History/index.tsx
+++ src/Pages/History/index.tsx
@@ -1,123 +1,112 @@
-import React from 'react';
-import {SafeAreaView, ScrollView, StyleSheet, View} from 'react-native';
-import {IconSendActive, IconShopActive, IconTopUpActive} from '../../Assets';
+import axios from 'axios';
+import React, {useCallback, useEffect, useState} from 'react';
+import {
+  Alert,
+  RefreshControl,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {
+  Fonts,
+  IconSendActive,
+  IconShopActive,
+  IconTopUpActive,
+} from '../../Assets';
 import {Gap, Header, LastestTransaction} from '../../Components';
+import {API_URL} from '../../Service/config';
+import {getData} from '../../Utils/LocalStorage';
 
 const History = () => {
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const formatRupiah = (amount: any) => {
+    return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+  };
+
+  useEffect(() => {
+    getHistory();
+  }, []);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      getHistory();
+      setRefreshing(false);
+      setPage(1);
+    }, 1000);
+  }, [refreshing]);
+
+  const getHistory = () => {
+    getData('token').then(token => {
+      axios
+        .get(`${API_URL}/transactions?page=${page}`, {
+          headers: {Authorization: `Bearer ${token.token}`},
+        })
+        .then(res => {
+          console.log('RESPONSE :', res);
+          if (page === 1) {
+            setData(res.data.data);
+          }
+          setRefreshing(false);
+        })
+        .catch(err => {
+          Alert.alert('Error', err.response, [{text: 'TUTUP'}]);
+          setRefreshing(false);
+        });
+    });
+  };
+
   return (
     <SafeAreaView style={styles.page}>
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={() => onRefresh()}
+          />
+        }>
         <Header title={'History'} />
         <View style={styles.container}>
           <View style={styles.contentLT}>
-            <LastestTransaction
-              icon={IconTopUpActive}
-              title={'TopUp'}
-              date={'Yesterday'}
-              amount={'+ 450.000'}
-            />
-            <LastestTransaction
-              icon={IconSendActive}
-              title={'Transfer'}
-              date={'Aug 27 2023'}
-              amount={'- 50.000'}
-            />
-            <LastestTransaction
-              icon={IconShopActive}
-              title={'Shop'}
-              date={'Sep 04 2023'}
-              amount={'- 100.000'}
-            />
-            <LastestTransaction
-              icon={IconTopUpActive}
-              title={'TopUp'}
-              date={'Yesterday'}
-              amount={'+ 450.000'}
-            />
-            <LastestTransaction
-              icon={IconSendActive}
-              title={'Transfer'}
-              date={'Aug 27 2023'}
-              amount={'- 50.000'}
-            />
-            <LastestTransaction
-              icon={IconShopActive}
-              title={'Shop'}
-              date={'Sep 04 2023'}
-              amount={'- 100.000'}
-            />
-            <LastestTransaction
-              icon={IconTopUpActive}
-              title={'TopUp'}
-              date={'Yesterday'}
-              amount={'+ 450.000'}
-            />
-            <LastestTransaction
-              icon={IconSendActive}
-              title={'Transfer'}
-              date={'Aug 27 2023'}
-              amount={'- 50.000'}
-            />
-            <LastestTransaction
-              icon={IconShopActive}
-              title={'Shop'}
-              date={'Sep 04 2023'}
-              amount={'- 100.000'}
-            />
-            <LastestTransaction
-              icon={IconTopUpActive}
-              title={'TopUp'}
-              date={'Yesterday'}
-              amount={'+ 450.000'}
-            />
-            <LastestTransaction
-              icon={IconSendActive}
-              title={'Transfer'}
-              date={'Aug 27 2023'}
-              amount={'- 50.000'}
-            />
-            <LastestTransaction
-              icon={IconShopActive}
-              title={'Shop'}
-              date={'Sep 04 2023'}
-              amount={'- 100.000'}
-            />
-            <LastestTransaction
-              icon={IconTopUpActive}
-              title={'TopUp'}
-              date={'Yesterday'}
-              amount={'+ 450.000'}
-            />
-            <LastestTransaction
-              icon={IconSendActive}
-              title={'Transfer'}
-              date={'Aug 27 2023'}
-              amount={'- 50.000'}
-            />
-            <LastestTransaction
-              icon={IconShopActive}
-              title={'Shop'}
-              date={'Sep 04 2023'}
-              amount={'- 100.000'}
-            />
-            <LastestTransaction
-              icon={IconTopUpActive}
-              title={'TopUp'}
-              date={'Yesterday'}
-              amount={'+ 450.000'}
-            />
-            <LastestTransaction
-              icon={IconSendActive}
-              title={'Transfer'}
-              date={'Aug 27 2023'}
-              amount={'- 50.000'}
-            />
-            <LastestTransaction
-              icon={IconShopActive}
-              title={'Shop'}
-              date={'Sep 04 2023'}
-              amount={'- 100.000'}
-            />
+            {data.length > 0 ? (
+              <View>
+                {data.map((item: any) => (
+                  <LastestTransaction
+                    key={item.id}
+                    icon={
+                      item.transaction_type.code === 'top_up'
+                        ? IconTopUpActive
+                        : item.transaction_type.code === 'transfer'
+                        ? IconSendActive
+                        : IconShopActive
+                    }
+                    title={
+                      item.transaction_type.code === 'top_up'
+                        ? 'Top Up'
+                        : item.transaction_type.code === 'transfer'
+                        ? 'Transfer'
+                        : 'Internet'
+                    }
+                    date={item.created_at}
+                    amount={
+                      (item.transaction_type.code === 'top_up' ? '+' : '-') +
+                      ' ' +
+                      formatRupiah(item.amount)
+                    }
+                  />
+                ))}
+              </View>
+            ) : (
+              <View style={styles.contentTitle}>
+                <Text style={styles.title}>Anda Belum Meliliki Transaksi</Text>
+              </View>
+            )}
           </View>
         </View>
         <Gap height={30} width={0} />
@@ -142,5 +131,16 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     paddingTop: 22,
     paddingHorizontal: 22,
+  },
+  contentTitle: {
+    alignItems: 'center',
+    marginBottom: 22,
+  },
+  title: {
+    fontSize: 14,
+    fontFamily: Fonts.Poppins[600],
+    color: '#A4A8AE',
+    textAlign: 'center',
+    maxWidth: 250,
   },
 });
